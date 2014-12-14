745e6210c212dcd6d3fea22e3bc0d68dea9975dc
Guillaume Fillon <guillaume@posteo.de>
2014-12-14 06:02:00 +0100

    Add custom inflections

--- webapp/config/initializers/inflections.rb
+++ webapp/config/initializers/inflections.rb
@@ -14,3 +14,8 @@
 # ActiveSupport::Inflector.inflections(:en) do |inflect|
 #   inflect.acronym 'RESTful'
 # end
+
+ActiveSupport::Inflector.inflections do |inflect|
+  inflect.irregular 'os', 'oses'
+  inflect.irregular 'api', 'apis'
+end
