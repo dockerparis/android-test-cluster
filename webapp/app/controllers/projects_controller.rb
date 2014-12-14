9e7fed7ffd2e5831192aa919c3ac33d5ed016e92
Guillaume Fillon <guillaume@posteo.de>
2014-12-14 06:02:51 +0100

    Update ProjectsController

--- webapp/app/controllers/projects_controller.rb
+++ webapp/app/controllers/projects_controller.rb
@@ -1,7 +1,14 @@
 class ProjectsController < ApplicationController
   def new
+  	@project = Project.new
   end
 
   def create
+  	@project = Project.new(params[:project])
+    if @project.save
+      redirect_to @project
+    else
+      render "new"
+    end
   end
 end
