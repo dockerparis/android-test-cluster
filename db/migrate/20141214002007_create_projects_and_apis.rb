6c6e9fed6a3cb95c790fc7f05243e7b175303b6b
Guillaume Fillon <guillaume@posteo.de>
2014-12-15 04:01:20 +0100

    Remove useless files

--- db/migrate/20141214002007_create_projects_and_apis.rb
+++
@@ -1,8 +0,0 @@
-class CreateProjectsAndApis < ActiveRecord::Migration
-  def change
-    create_table :projects_and_apis, id: false do |t|
-      t.belongs_to :projects
-      t.belongs_to :apis
-    end
-  end
-end
